26bddf51f8c08a17e60188abc875bb2ac2eeaaea
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-05-16 15:49:07 +0100

    Init project

---
+++ src/main/java/com/codewithpcodes/anistream/AniStreamApplication.java
@@ -0,0 +1,13 @@
+package com.codewithpcodes.anistream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AniStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AniStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithpcodes/anistream/AniStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithpcodes.anistream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AniStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
