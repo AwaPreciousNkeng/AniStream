446d6ce1e3a2980835bc9e65a8436e8a4d15ede5
AwaPreciousNkeng <awaprecious3t@gmail.com>
2026-05-16 16:25:52 +0100

    system configs

--- src/main/java/com/codewithpcodes/anistream/AniStreamApplication.java
+++ src/main/java/com/codewithpcodes/anistream/AniStreamApplication.java
@@ -1,9 +1,19 @@
 package com.codewithpcodes.anistream;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableCaching
+@EnableRetry
+@EnableScheduling
+@EnableBatchProcessing
 public class AniStreamApplication {
 
     public static void main(String[] args) {
